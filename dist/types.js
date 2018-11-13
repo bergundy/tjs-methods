bd390ac135bbbf77dd329f5e0a9eb1a29b2ac5c2
Roey Berman <roey.berman@gmail.com>
2018-11-13 14:58:06 +0200

    Add missing compiled files

---
+++ dist/types.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Role;
+(function (Role) {
+    Role["ALL"] = "all";
+    Role["CLIENT"] = "client";
+    Role["SERVER"] = "server";
+})(Role = exports.Role || (exports.Role = {}));
+//# sourceMappingURL=types.js.map
